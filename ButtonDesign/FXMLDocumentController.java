8f547aca69aa29f66fb189a3a2b3d5f224771957
Houari Zegai <houarizegai14@gmail.com>
2018-03-28 19:51:12 +0100

    Adding button design

---
+++ ButtonDesign/FXMLDocumentController.java
@@ -0,0 +1,16 @@
+
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLDocumentController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+}
