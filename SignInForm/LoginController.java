981280c40e6d7bf8886df3b64671f64545577a67
Houari Zegai <houarizegai14@gmail.com>
2018-04-03 14:45:50 +0100

    Add sign in folder

---
+++ SignInForm/LoginController.java
@@ -0,0 +1,13 @@
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
